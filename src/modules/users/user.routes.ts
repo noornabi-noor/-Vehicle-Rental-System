bb592a2835eafe63ec3a92bc4539ddcc000d34db
Noornabi <noornabimd9891@gmail.com>
2025-12-20 22:50:42 +0600

    vercel.json file added and final check

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/", userController.createUser);
 router.get("/", auth("admin"), userController.getUsers);
-router.get("/:userId", userController.getSingleUser);
+router.get("/:userId", auth("admin"), userController.getSingleUser);
 router.put("/:userId", auth("admin", "customer"), userController.updateUser);
 router.delete("/:userId", auth("admin"), userController.deleteUser);
 
